c9a10cccb215549dab3588252d14718302e5f0f0
jacksonvalereal <jacksonvalereal@gmail.com>
2017-08-03 22:20:59 -0300

    Update automacao_residencial.js

--- automacao_residencial.js
+++ automacao_residencial.js
@@ -3,7 +3,7 @@ function AlteraRele1(){
     if(estado === "0"){
         document.getElementById("estado").innerHTML="1";
         document.getElementById("rele").innerHTML="<div class='rele1_ligado'></div>";
-		document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desliga</a>";
+	document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desliga</a>";
     } else {
         document.getElementById("estado").innerHTML="0";
         document.getElementById("rele").innerHTML="<div class='rele1_desligado'></div>";
@@ -16,7 +16,7 @@ function AlteraRele2(){
     if(estado_2 === "0"){
         document.getElementById("estado_2").innerHTML="1";
         document.getElementById("rele_2").innerHTML="<div class='rele2_ligado'></div>";
-		document.getElementById("botao_2").innerHTML="<a href='/?2_desligar' class='botao_2'>Desliga</a>";
+	document.getElementById("botao_2").innerHTML="<a href='/?2_desligar' class='botao_2'>Desliga</a>";
     } else {
         document.getElementById("estado_2").innerHTML="0";
         document.getElementById("rele_2").innerHTML="<div class='rele2_desligado'></div>";
@@ -27,7 +27,7 @@ function AlteraRele3(){
     if(estado_3 === "0"){
         document.getElementById("estado_3").innerHTML="1";
         document.getElementById("rele_3").innerHTML="<div class='rele3_ligado'></div>";
-		document.getElementById("botao_3").innerHTML="<a href='/?3_desligar' class='botao_3'>Desliga</a>";
+	document.getElementById("botao_3").innerHTML="<a href='/?3_desligar' class='botao_3'>Desliga</a>";
     } else {
         document.getElementById("estado_3").innerHTML="0";
         document.getElementById("rele_3").innerHTML="<div class='rele3_desligado'></div>";
@@ -38,7 +38,7 @@ function AlteraRele4(){
     if(estado_4 === "0"){
         document.getElementById("estado_4").innerHTML="1";
         document.getElementById("rele_4").innerHTML="<div class='rele4_ligado'></div>";
-		document.getElementById("botao_4").innerHTML="<a href='/?4_desligar' class='botao_4'>Desliga</a>";
+	document.getElementById("botao_4").innerHTML="<a href='/?4_desligar' class='botao_4'>Desliga</a>";
     } else {
         document.getElementById("estado_4").innerHTML="0";
         document.getElementById("rele_4").innerHTML="<div class='rele4_desligado'></div>";
